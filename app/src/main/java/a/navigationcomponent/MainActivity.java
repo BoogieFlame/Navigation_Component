ae4daeeef32e4ca8d0aab4c7704908203736ea8d
BoogieFlame <kastor.vip@gmail.com>
2020-12-08 15:10:14 +0700

    Добавление Fragment + ViewModel

---
+++ app/src/main/java/a/navigationcomponent/MainActivity.java
@@ -0,0 +1,21 @@
+package a.navigationcomponent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import a.navigationcomponent.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/a/navigationcomponent/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package a.navigationcomponent.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
